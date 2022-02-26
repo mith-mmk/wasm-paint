25fa16e203cfcfaf6d826dbe699ce391fc62d5e9
mith@mmk <you@example.com>
2022-02-27 08:34:59 +0900

    line update

--- src/paint/line.rs
+++ src/paint/line.rs
@@ -20,67 +20,36 @@ fn _point (canvas: &mut Canvas, x: i32, y: i32, r :u8, g :u8, b :u8, a :u8) {
 // line no antialias
 pub fn line ( canvas: &mut Canvas, x0: i32, y0: i32, x1: i32, y1: i32 , color: u32) {
     let (red, green, blue, _) = color_taple(color);
-    let sx;
-    let ex;
-    let sy;
-    let ey;
-    if x0 > x1 {
-        sx = x1;
-        ex = x0;
-        sy = y1;
-        ey = y0;
 
-    } else {
-        sx = x0;
-        ex = x1;
-        sy = y0;
-        ey = y1;
-    }
-    if ex == sx {
-        let x = sx;
-        if sy <= ey {
-            for y in sy..ey + 1 {
-                _point(canvas, x as i32, y as i32, red, green, blue, 0xff);
-            }
-        } else {
-            for y in ey..sy + 1 {
-                _point(canvas, x as i32, y as i32, red, green, blue, 0xff);
-            }
-        }
-    } else if ey == sy {
-        for x in sx..ex + 1 {
-            _point(canvas, x as i32, sy as i32, red, green, blue, 0xff);
-        }
-    } else {
-        let delta = (ey -sy) as f32 / (ex - sx) as f32;
+    let dx = (x0 - x1).abs();
+    let dy = (y0 - y1).abs();
 
-        if -1.0 < delta && delta < 1.0 {
-            let mut fy = sy as f32;
-            for x in sx..ex + 1 {
-                let y = fy.round() as i32;
-                _point(canvas, x as i32, y as i32, red, green, blue, 0xff);
-                fy = fy + delta;
-            }
-        } else {
-            let mut fx = sx as f32;
-            let mut y = sy;
-            let dy;
-            let delta;
-            if sy <= ey {
-                dy = 1;
-                delta = (ex - sx) as f32 / (ey -sy) as f32 ;
-            } else {
-                delta = - (ex - sx) as f32 / (ey -sy) as f32 ;
-                dy = -1;
-            }
-            while y != ey {
-                let x = fx.round() as i32;
-                _point(canvas, x as i32, y as i32, red, green, blue, 0xff);
-                fx = fx + delta;
-                y = y + dy;
-            }
-            _point(canvas, ex as i32, ey as i32, red, green, blue, 0xff);
+    let step_x = if (x0 < x1) { 1 } else { -1 };
+    let step_y = if (y0 < y1) { 1 } else { -1 };
 
+    let mut err = dx - dy;
+
+    let mut x = x0;
+    let mut y = y0;
+
+    loop {
+        _point(canvas, x as i32, y as i32, red, green, blue, 0xff);
+
+        if x == x1 && y == y1 {
+            break;
         }
+        
+        let err2 = err * 2;
+
+        if err2 > -dy  {
+            err = err - dy;
+            x = x + step_x;
+        }
+
+        if err2 < dx  {
+            err = err + dx;
+            y = y + step_y;
+        }
+
     }
 }
